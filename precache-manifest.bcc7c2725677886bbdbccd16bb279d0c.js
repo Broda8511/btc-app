2fa467070f4f5a9f1a732514829d1ccd9d807be0
Adam Megger <adammegger@int.pl>
2020-09-09 14:17:26 +0200

    Updates

---
+++ precache-manifest.bcc7c2725677886bbdbccd16bb279d0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f193a23422c58dc97ae050ea48d189d",
+    "url": "/btc-app/index.html"
+  },
+  {
+    "revision": "821b0e71e61e24e31dc9",
+    "url": "/btc-app/static/js/2.32193875.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/btc-app/static/js/2.32193875.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "572a7493bb55a02dfb7e",
+    "url": "/btc-app/static/js/main.11ec5815.chunk.js"
+  },
+  {
+    "revision": "220938b4767858f2c37c",
+    "url": "/btc-app/static/js/runtime-main.9b508b67.js"
+  }
+]);
